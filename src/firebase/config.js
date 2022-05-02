54ca25a0e0d4a8cb4c50b1c3c29212052c495e49
SayidRadhin <pmradhin@gmail.com>
2022-05-02 11:14:22 +0530

    created firebase context

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+import 'firebase/auth'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCouAgndOQPYabc52fbeLeJgTnW1Wcm2cI",
+    authDomain: "olx-clone-9ce91.firebaseapp.com",
+    projectId: "olx-clone-9ce91",
+    storageBucket: "olx-clone-9ce91.appspot.com",
+    messagingSenderId: "559659239491",
+    appId: "1:559659239491:web:5c07c9209b754e3eb0acf6",
+    measurementId: "G-36SG83DXDR"
+  };
+
+  export default  firebase.initializeApp(firebaseConfig)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {FirebaseContext} from './store/FirebaseContext'
+import firebase from "./firebase/config";
+ReactDOM.render(
+    <FirebaseContext.Provider value={{firebase}}>
+        <App />
+    </FirebaseContext.Provider>
 
-ReactDOM.render(<App />, document.getElementById('root'));
+, document.getElementById('root'));
